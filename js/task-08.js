9614dc198af41064a27f6425637a55ac22e02511
Ruslan Kvasha <ruslankv4@gmail.com>
2022-07-23 18:52:12 +0200

    done task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const refs = {
+  form: document.querySelector('.login-form'),
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (mail === '' || password === '') {
+    alert('Fill up all fields of form');
+  }
+
+  const formData = {
+    mail,
+    password,
+  };
+
+  console.log(formData);
+
+  refs.form.reset();
+}
