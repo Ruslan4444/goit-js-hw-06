3c45534ea96893d0a6c4dba5cf89c5c5f015698d
Ruslan Kvasha <ruslankv4@gmail.com>
2022-07-23 19:12:14 +0200

    done task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  button: document.querySelector('.change-color'),
+  span: document.querySelector('.color'),
+};
+
+refs.button.addEventListener('click', onButtonChangeBodyBgColor);
+
+function onButtonChangeBodyBgColor() {
+  const currentColor = getRandomHexColor();
+  document.body.style.background = currentColor;
+  refs.span.textContent = currentColor;
+}
